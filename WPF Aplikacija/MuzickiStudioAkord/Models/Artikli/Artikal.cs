7b1a8fd3a3d931af0fb5928865f88df5cbee5a6f
Adnan Alibegovic <aalibegovi2@etf.unsa.ba>
2015-05-12 01:17:39 +0200

    dodani modeli

---
+++ WPF Aplikacija/MuzickiStudioAkord/Models/Artikli/Artikal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuzickiStudioAkord.Models
+{
+    [Serializable]
+    public class Artikal
+    {
+        private int serijskiBroj;
+        public int SerijskiBroj
+        {
+            get { return serijskiBroj; }
+            set { serijskiBroj = value; }
+        }
+
+        private string naziv;
+        public string Naziv
+        {
+            get { return naziv; }
+            set { naziv = value; }
+        }
+
+        private double cijena;
+        public double Cijena
+        {
+            get { return cijena; }
+            set { cijena = value; }
+        }
+
+        private Specifikacija spec;
+        internal Specifikacija Spec
+        {
+            get { return spec; }
+            set { spec = value; }
+        }
+
+        private string slikaPath;
+
+        public string SlikaPath
+        {
+            get { return slikaPath; }
+            set { slikaPath = value; }
+        }
+
+
+    }
+}
