fb3f8d796fe73291a8a3dbc64e8300cf91d9b09f
Dino Babahmetovic <dbabahmeto1@etf.unsa.ba>
2015-05-14 04:07:22 +0200

    malo samo sredio sastanke :P

--- WPF Aplikacija/MuzickiStudioAkord/Models/Sistem/Sastanak.cs
+++ WPF Aplikacija/MuzickiStudioAkord/Models/Sistem/Sastanak.cs
@@ -8,14 +8,43 @@ namespace MuzickiStudioAkord.Models
 {
     public class Sastanak
     {
-        public int Id_sastanak { get; set; }
-        public string Naziv { get; set; }
-        public DateTime Datum { get; set; }
-        public string Opis { get; set; }
-        public Klijent Klijent { get; set; }
+        private int id_sastanak; 
+        public int Id_sastanak 
+        {
+            get { return id_sastanak; }
+            set { id_sastanak = value; }
+        }
+        private string naziv;
+        public string Naziv
+        {
+            get { return naziv; }
+            set { naziv = value; }
+        }
+        private DateTime datum;
+        public DateTime Datum
+        {
+            get { return datum; }
+            set { datum = value; }
+        }
+        private string opis;
+        public string Opis
+        {
+            get { return opis; }
+            set { opis = value; }
+        }
+        private Klijent klijent;
+        public Klijent Klijent
+        {
+            get { return klijent; }
+            set { klijent = value; }
+        }
         public Sastanak(int id, string naziv, DateTime datum, string opis, Klijent klijent)
         {
-
+            this.Id_sastanak = id;
+            this.Naziv = naziv;
+            this.Datum = datum;
+            this.Opis = opis;
+            this.Klijent = klijent;
         }
     }
 }
