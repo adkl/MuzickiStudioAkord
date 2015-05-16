bd7bca0ee41e6596145199a1302477604bd0f40a
Dino Babahmetovic <dbabahmeto1@etf.unsa.ba>
2015-05-16 15:16:12 +0200

    update Inventory

--- WPF Aplikacija/MuzickiStudioAkord/Models/Sistem/Inventory.cs
+++ WPF Aplikacija/MuzickiStudioAkord/Models/Sistem/Inventory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MuzickiStudioAkord.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,15 +15,28 @@ namespace MuzickiStudioAkord.Models
             get { return artikli; }
             set { artikli = value; }
         }
-
+        private DataBaseArtikli baza;
+        public DataBaseArtikli Baza
+        {
+            get { return baza; }
+            set { baza = value; }
+        }
+        public Inventory(string password, string server = "db4free.net", string username = "studioakord", string database = "muzickistudio")
+        {
+            this.Baza = new DataBaseArtikli(password, server, username, database);
+            this.Artikli = baza.dajSve();
+        }
+        //ovaj je konstruktor ako imas bas neke posebne artikle koji ti samo trebaju xD
         public Inventory(List<Artikal> _artikli)
         {
             this.Artikli = _artikli;
         }
-        public Inventory()
+        public void osvjeziInventory()
         {
-
+            Artikli.Clear();
+            Artikli = baza.dajSve();
         }
+        
 
 
     }
